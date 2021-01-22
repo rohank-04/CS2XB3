cbc8454d1b0cc257834f3a41692e13c073b28803
amujtaba00 <amujtaba00@gmail.com>
2021-01-22 23:16:40 +0500

    Changed code.py function

--- code.py
+++ code.py
@@ -1,9 +1,20 @@
 def are_valid_groups(n,g):
 
+    empty = []
+
+    for student in n:
+        if student in empty:
+            return False
+        else:
+            empty += [student]
+
     curr = False
     for student in n:
         curr = False
         for group in g:
+            if ((len(group) != 3) and (len(group) != 2)):
+                # Group size is not 2 or 3
+                return False
             for groupMember in group:
                 if student == groupMember:
                     curr = True
@@ -14,3 +25,10 @@ def are_valid_groups(n,g):
         if curr == False:
             return False
     return True
+
+
+
+
+n = [1,2,3,4,5,6]
+g = [[1,2],[3,4],[5,6,7]]
+print(are_valid_groups(n,g))
