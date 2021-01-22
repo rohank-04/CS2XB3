20a52571993040eaf28c3d1394b9e1415fecef22
rohank-04 <karnikr@mcmaster.ca>
2021-01-22 22:09:20 +0400

    Changes in code.py

--- code.py
+++ code.py
@@ -1,9 +1,9 @@
 def are_valid_groups(n,g):
 
-    curr = False
+    
     for student in n:
         curr = False
-        for group in g:
+       
             for groupMember in group:
                 if student == groupMember:
                     curr = True
@@ -13,4 +13,4 @@ def are_valid_groups(n,g):
             
         if curr == False:
             return False
-    return True
+    
